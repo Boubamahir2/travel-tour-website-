865baf42be711c7286a7bfccb4d103c24884b3bf
Boubamahir2 <amamahir1@gmail.com>
2021-09-11 20:39:29 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,111 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.nav-bar');
+let videoBtn = document.querySelectorAll('.vid-btn');
+
+let login = document.querySelector('#login');
+let register =document.querySelector('#register');
+
+
+
+window.onscroll = () =>{
+  searchBtn.classList.remove('fa-times');
+  searchBar.classList.remove('active');
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+  loginForm.classList.remove('active');
+  
+}
+
+const registerNow = ()=> {
+  login.style.display = "none";
+  register.style.display = "block";
+
+}
+
+const loginNow = ()=> {
+  register.style.display = "none";
+  login.style.display = "block"
+}
+
+searchBtn.addEventListener('click', () =>{
+  searchBtn.classList.toggle('fa-times');
+  searchBar.classList.toggle('active');
+
+});
+
+menu.addEventListener('click', () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+
+});
+
+formBtn.addEventListener('click', () =>{
+  loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () =>{
+  loginForm.classList.remove('active');
+});
+
+
+videoBtn.forEach(btn => {
+  btn.addEventListener('click', ()=>{
+    document.querySelector('.video-controls .active').classList.remove('active');
+    btn.classList.add('active');
+    let src = btn.getAttribute('data-src');
+    document.querySelector('#video-slider').src = src;
+  } )
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+});
+
+
+
+var swiper = new Swiper(".brand-slider", {
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+  },
+  breakpoints: {
+      450: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      991: {
+        slidesPerView: 4,
+      },
+      1200: {
+        slidesPerView: 5,
+      },
+    },
+});
+
+
+
